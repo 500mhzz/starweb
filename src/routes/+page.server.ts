6a0024d451007933962202ff3f6c6f6ee119b29e
500mhzz <its500mhz@proton.me>
2024-06-08 14:23:28 +0300

    Added multi channel support and changed theme

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -7,54 +7,65 @@ export async function load({ fetch, url }) {
 			client.once('ready', resolve);
 		});
 	}
-	const guild = await client.guilds.cache.get(env.DISCORD_GUILD_ID);
-	const channel = await guild?.channels.cache.get(env.CHANNEL_ID);
 
 	const postsPerPage = 20;
 	const page = Number(url.searchParams.get('page')) || 1;
 	let messages;
 	let posts: any[] = [];
 
-	if (channel?.isTextBased()) {
-		if (page === 1) {
-			messages = await channel.messages.fetch({ limit: postsPerPage });
-		} else {
-			// Calculate the number of messages to skip
-			const skipMessages = (page - 1) * postsPerPage;
-			let lastMessageId = await channel.messages
-				.fetch({ limit: 1 })
-				.then((messages) => messages.first()?.id);
+	const channelId = url.searchParams.get('channelId') || env.CHANNEL_ID;
 
-			// Fetch messages in batches to skip the correct amount
-			for (let i = 0; i < skipMessages; i += postsPerPage) {
-				const fetchLimit = Math.min(postsPerPage, skipMessages - i);
-				messages = await channel.messages.fetch({ limit: fetchLimit, before: lastMessageId });
-				lastMessageId = messages.last()?.id;
-				if (messages.size < fetchLimit) break; // Break if we've reached the end of the messages
-			}
+	const guild = client.guilds.cache.get(env.DISCORD_GUILD_ID);
+	const channel = guild?.channels.cache.get(channelId);
 
-			// Now fetch the actual page of messages
-			messages = await channel.messages.fetch({ limit: postsPerPage, before: lastMessageId });
+	if (!channel?.isTextBased())
+		return {
+			status: 404,
+			error: new Error('Channel not found')
+		};
+
+	if (page !== 1) {
+		// Calculate the number of messages to skip
+		const skipMessages = (page - 1) * postsPerPage;
+		let lastMessageId = await channel.messages
+			.fetch({ limit: 1 })
+			.then((messages) => messages.first()?.id);
+
+		// Fetch messages in batches to skip the correct amount
+		for (let i = 0; i < skipMessages; i += postsPerPage) {
+			const fetchLimit = Math.min(postsPerPage, skipMessages - i);
+			messages = await channel.messages.fetch({ limit: fetchLimit, before: lastMessageId });
+			lastMessageId = messages.last()?.id;
+			if (messages.size < fetchLimit) break; // Break if we've reached the end of the messages
 		}
 
-		messages.forEach((message) => {
-            const embedsWithTitleAndAttachment = message.embeds.filter(
-                (embed) => embed.author && embed.image
-            );
-            embedsWithTitleAndAttachment.forEach((embed) => {
-                // Calculate total reactions for the message
-                const totalReactions = Array.from(message.reactions.cache.values()).reduce((acc, reaction) => acc + reaction.count, 0);
-                posts.push({
-                    id: message.id,
-                    author: embed.author?.name,
-                    image: embed.image?.url,
-                    description: embed.description,
-                    timestamp: message.createdTimestamp,
-                    reactions: totalReactions // Add the total reactions count here
-                });
-            });
-        });
+		// Now fetch the actual page of messages
+		messages = await channel.messages.fetch({ limit: postsPerPage, before: lastMessageId });
+	} else {
+		messages = await channel.messages.fetch({ limit: postsPerPage });
 	}
+
+	messages.forEach((message) => {
+		const embedsWithTitleAndAttachment = message.embeds.filter(
+			(embed) => embed.author && embed.image
+		);
+		embedsWithTitleAndAttachment.forEach((embed) => {
+			// Calculate total reactions for the message
+			const totalReactions = Array.from(message.reactions.cache.values()).reduce(
+				(acc, reaction) => acc + reaction.count,
+				0
+			);
+			posts.push({
+				id: message.id,
+				author: embed.author?.name,
+				image: embed.image?.url,
+				description: embed.description,
+				timestamp: message.createdTimestamp,
+                url: message.url,
+				reactions: totalReactions // Add the total reactions count here
+			});
+		});
+	});
 
 	return {
 		page,
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,91 @@
 <script lang="ts">
-    import '../app.css';
-    export let data: {
-        posts: [
-            {
-                id: string;
-                author: string;
-                image: string;
-                description: string;
-                timestamp: string;
-                reactions: number;
-            }
-        ];
-        page: number;
-    };
-    console.log(data);
+	import '../app.css';
+	import { goto } from '$app/navigation';
+
+	interface Post {
+		id: string;
+		author: string;
+		image: string;
+		description: string;
+		timestamp: string;
+		reactions: number;
+		url: string;
+	}
+
+	interface Data {
+		posts: Post[];
+		page: number;
+	}
+
+	interface Channel {
+		name: string;
+		id: string;
+	}
+
+	export let data: Data;
+
+	console.log(data);
+
+	const channels: Channel[] = [
+		{
+			name: '#posts',
+			id: '1248927207326482462'
+		},
+		{
+			name: '#img',
+			id: '1216453543234502707'
+		}
+	];
 </script>
 
 <main class="mx-4 sm:mx-12 md:mx-48 lg:mx-96 mt-5 flex flex-col items-center justify-center p-5">
-    <div class="mb-5">
-        <h1 class="text-3xl sm:text-4xl font-bold">Starboard image thingy</h1>
-        <p class="text-zinc-600">
-            Coded by <a class="text-blue-500 hover:underline" href="https://500mhz.xyz">500mhz</a>
-        </p>
-    </div>
-    <div class="flex flex-wrap items-center justify-center gap-2 rounded-lg p-5">
-        {#each data.posts as item}
-            <div class="bg-neutral-900/35 border border-white/10 rounded-lg p-3">
-                <img src={item?.image} alt={item?.author} class="w-full sm:h-80 rounded-lg" />
-                <h1 class="text-xl sm:text-2xl font-bold mt-2">{item?.author}</h1>
-                {item.reactions}
-            </div>
-        {/each}
-    </div>
+	<div class="mb-5 bg-neutral-900/35 border border-white/10 w-full rounded-lg px-10 py-10">
+		<h1 class="text-3xl sm:text-4xl font-bold">Starboard image thingy</h1>
+		<p class="text-zinc-600">
+			Coded by <a class="text-blue-500 hover:underline" href="https://500mhz.xyz">500mhz</a>
+		</p>
+	</div>
+	<div class="flex flex-row w-full bg-neutral-900/35 border border-white/10 rounded-lg p-3">
+		{#each channels as channel}
+			<a
+				href="/?page={data.page}&channelId={channel.id}"
+				class="bg-neutral-900/35 border border-white/10 rounded-lg px-5 py-2 ml-2"
+			>
+				<p>{channel.name}</p>
+			</a>
+		{/each}
+	</div>
 
-    <div class="flex justify-center mt-5 gap-2">
-        <button
-            class="text-white px-4 py-2 sm:px-5 sm:py-2 rounded-lg bg-neutral-900/35 border border-white/10"
-            on:click={() => (window.location.href = `/?page=${data.page - 1}`)}>Previous</button
-        >
-        <button
-            class="text-white px-4 py-2 sm:px-5 sm:py-2 rounded-lg bg-neutral-900/35 border border-white/10"
-            on:click={() => (window.location.href = `/?page=${data.page + 1}`)}>Next</button
-        >
-    </div>
+	<div class="flex flex-wrap items-center justify-center gap-2 rounded-lg mt-5 w-full">
+		{#each data.posts as item}
+			<a href={item.url}>
+				<div
+					class="bg-neutral-900/35 hover:bg-neutral-900/70 border border-white/10 rounded-lg p-3"
+				>
+					<img
+						src={item?.image}
+						alt={item?.author}
+						class="w-full sm:max-h-64 rounded-lg object-cover"
+					/>
+					<div class="flex flex-row items-center justify-between mt-2">
+						<p class="text-xl sm:text-2xl font-bold">{item?.author}</p>
+						<p class="bg-neutral-900/35 border border-white/10 rounded-lg px-2 py-1 cursor-pointer">
+							⭐ {item.reactions}
+						</p>
+					</div>
+				</div>
+			</a>
+		{/each}
+	</div>
+
+	<div class="flex justify-center mt-5 gap-2">
+		<button
+			class="text-white px-4 py-2 sm:px-5 sm:py-2 rounded-lg bg-neutral-900/35 border border-white/10"
+			on:click={() => (window.location.href = `/?page=${data.page - 1}`)}>Previous</button
+		>
+		<button
+			class="text-white px-4 py-2 sm:px-5 sm:py-2 rounded-lg bg-neutral-900/35 border border-white/10"
+			on:click={() => (window.location.href = `/?page=${data.page + 1}`)}>Next</button
+		>
+	</div>
 </main>
